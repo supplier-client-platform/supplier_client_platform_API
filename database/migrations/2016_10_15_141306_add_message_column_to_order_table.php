d9889dc4553cadb036453965de173049753d43a5
rishanthakumarrasarathinam <rishanthakumar@gmail.com>
2016-10-16 00:23:18 +0530

    Fixed re declared migration.

--- database/migrations/2016_10_15_141306_add_message_column_to_order_table.php
+++ database/migrations/2016_10_15_141306_add_message_column_to_order_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddColumnsToOrderTable extends Migration
+class AddMessageColumnToOrderTable extends Migration
 {
     /**
      * Run the migrations.
