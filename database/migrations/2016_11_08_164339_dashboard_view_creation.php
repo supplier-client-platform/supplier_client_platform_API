402aa67f6735283365d75fa3be85a48db212f800
Nilesh Jayanandana <nilesh.jayanandana@yahoo.com>
2016-11-08 22:15:37 +0530

    migration error fix

---
+++ database/migrations/2016_11_08_164339_dashboard_view_creation.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class DashboardViewCreation extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        DB::statement("DROP VIEW IF EXISTS view_dashboard_orders");
+        DB::statement(
+            "CREATE VIEW view_dashboard_orders
+            AS
+            select
+            coalesce(`b`.`orders`,0) AS `orders`,
+            `t`.`status` AS `status`,
+            coalesce(`b`.`supplier_id`,0) AS `supplier_id`,
+            coalesce(`b`.`gross_total`,0) AS `gross_total`,
+            coalesce(`b`.`net_total`,0) AS `net_total`,
+            coalesce(`b`.`discount`,0) AS `discount`,
+            monthname(str_to_date(`t`.`month`,'%m')) AS `month_name`,
+            `t`.`month` AS `month` 
+                from view_dashboard_months `t` 
+                    left join view_dashboard_order_list `b` on(((`t`.`month` = `b`.`month`) and (`t`.`status` = `b`.`status`)));"
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        DB::statement("DROP VIEW IF EXISTS view_dashboard_orders");
+    }
+}
