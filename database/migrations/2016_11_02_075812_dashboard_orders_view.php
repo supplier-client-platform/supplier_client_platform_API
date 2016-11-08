d00f019d8d5e90d9e9dc70831f9548e02c0972cd
Nilesh Jayanandana <nilesh.jayanandana@yahoo.com>
2016-11-08 15:24:07 +0530

    migration issue fixed

--- database/migrations/2016_11_02_075812_dashboard_orders_view.php
+++ database/migrations/2016_11_02_075812_dashboard_orders_view.php
@@ -13,23 +13,14 @@ class DashboardOrdersView extends Migration
     public function up()
     {
         //
-        DB::statement("DROP VIEW IF EXISTS view_dashboard_orders");
+        DB::statement("DROP VIEW IF EXISTS view_dashboard_months");
         DB::statement(
-            "CREATE VIEW view_dashboard_orders
-            AS
-            select
-            coalesce(`b`.`orders`,0) AS `orders`,
-            `t`.`status` AS `status`,
-            coalesce(`b`.`supplier_id`,0) AS `supplier_id`,
-            coalesce(`b`.`gross_total`,0) AS `gross_total`,
-            coalesce(`b`.`net_total`,0) AS `net_total`,
-            coalesce(`b`.`discount`,0) AS `discount`,
-            monthname(str_to_date(`t`.`month`,'%m')) AS `month_name`,
-            `t`.`month` AS `month` 
-                from (((select 
+            "CREATE VIEW view_dashboard_months
+        AS
+        select 
                     `f`.`month` AS `month`,
                     `s`.`status` AS `status`
-                        from ((select 1 AS `month`
+                        from (select 1 AS `month`
                         union select 2 AS `month`
                         union select 3 AS `month` 
                         union select 4 AS `month` 
@@ -45,7 +36,23 @@ class DashboardOrdersView extends Migration
                         'Completed' AS `status` 
                             union select 'Pending' AS `status`
                             union select 'Accepted' AS `status` 
-                            union select 'Rejected' AS `status`) `s`))) `t` 
+                            union select 'Rejected' AS `status`) `s`;"
+        );
+
+        DB::statement("DROP VIEW IF EXISTS view_dashboard_orders");
+        DB::statement(
+            "CREATE VIEW view_dashboard_orders
+            AS
+            select
+            coalesce(`b`.`orders`,0) AS `orders`,
+            `t`.`status` AS `status`,
+            coalesce(`b`.`supplier_id`,0) AS `supplier_id`,
+            coalesce(`b`.`gross_total`,0) AS `gross_total`,
+            coalesce(`b`.`net_total`,0) AS `net_total`,
+            coalesce(`b`.`discount`,0) AS `discount`,
+            monthname(str_to_date(`t`.`month`,'%m')) AS `month_name`,
+            `t`.`month` AS `month` 
+                from view_dashboard_months `t` 
                     left join (select 
                         month(date_format(`a`.`created_at`,'%Y-%m-01')) AS `month`,
                         monthname(date_format(`a`.`created_at`,'%Y-%m-01')) AS `month_name`,
@@ -54,7 +61,7 @@ class DashboardOrdersView extends Migration
                         sum(`a`.`net_total`) AS `net_total`,sum(`a`.`discount`) AS `discount` 
                     from `scp`.`order` `a`
                     where (`a`.`created_at` > (now() - interval 6 month))
-                    group by date_format(`a`.`created_at`,'%Y-%m-01'),`a`.`supplier_id`,`a`.`status`) `b` on(((`t`.`month` = `b`.`month`) and (`t`.`status` = `b`.`status`))));"
+                    group by date_format(`a`.`created_at`,'%Y-%m-01'),`a`.`supplier_id`,`a`.`status`) `b` on(((`t`.`month` = `b`.`month`) and (`t`.`status` = `b`.`status`)));"
         );
     }
 
@@ -67,5 +74,6 @@ class DashboardOrdersView extends Migration
     {
         //
         DB::statement("DROP VIEW IF EXISTS view_dashboard_orders");
+        DB::statement("DROP VIEW IF EXISTS view_dashboard_months");
     }
 }
