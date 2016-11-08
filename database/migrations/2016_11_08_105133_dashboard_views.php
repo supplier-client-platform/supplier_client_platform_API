7d4a306ffd55be7a8a260768e80464ec04caf103
Nilesh Jayanandana <nilesh.jayanandana@yahoo.com>
2016-11-08 22:18:54 +0530

    migration error fix

--- database/migrations/2016_11_08_105133_dashboard_views.php
+++ database/migrations/2016_11_08_105133_dashboard_views.php
@@ -80,22 +80,7 @@ class DashboardViews extends Migration
             group by date_format(`a`.`created_at`,'%Y-%m-01'),`a`.`supplier_id`,`a`.`status`;"
         );
 
-        DB::statement("DROP VIEW IF EXISTS view_dashboard_orders");
-        DB::statement(
-            "CREATE VIEW view_dashboard_orders
-            AS
-            select
-            coalesce(`b`.`orders`,0) AS `orders`,
-            `t`.`status` AS `status`,
-            coalesce(`b`.`supplier_id`,0) AS `supplier_id`,
-            coalesce(`b`.`gross_total`,0) AS `gross_total`,
-            coalesce(`b`.`net_total`,0) AS `net_total`,
-            coalesce(`b`.`discount`,0) AS `discount`,
-            monthname(str_to_date(`t`.`month`,'%m')) AS `month_name`,
-            `t`.`month` AS `month` 
-                from view_dashboard_months `t` 
-                    left join view_dashboard_order_list `b` on(((`t`.`month` = `b`.`month`) and (`t`.`status` = `b`.`status`)));"
-        );
+
     }
 
     /**
