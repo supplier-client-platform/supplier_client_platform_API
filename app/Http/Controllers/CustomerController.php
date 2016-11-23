1cdd53e4b1c1b56df95bf1f401d893db7203ce83
Nilesh Jayanandana <nilesh.jayanandana@yahoo.com>
2016-11-23 12:10:01 +0530

    fixed a typo

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -13,19 +13,19 @@ class CustomerController extends Controller
 
     public function create(Request $request){
 
-            try {
+//            try {
                 $data = $request->all();
                 $customer = new Customer;
                 $customer->name = $data['displayName'];
                 //$customer->address = $data['address'];
                 $customer->email = $data['email'];
                 $customer->contact = $data['telephone'];
-                $customer->udid = $data['uuid'];
+                $customer->udid = $data['uid'];
 
                 $customer->save();
                 return response()->json(['status' => 'success', 'customer' => $customer], 200);
-            } catch (Exception $e){
-                return response()->json(['status' => 'failed', 'message'=>$e], 400);
-            }
+//            } catch (Exception $e){
+//                return response()->json(['status' => 'failed', 'message'=>$e], 400);
+//            }
     }
 }
